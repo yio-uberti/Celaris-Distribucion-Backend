a8da96346a631f7cde88ac85aebdd177e6df926a
yio-uberti <164667770+yio-uberti@users.noreply.github.com>
2026-05-17 19:20:29 -0300

    modificaciones de firebase

--- src/main/java/com/api/security/FirebaseConfig.java
+++ src/main/java/com/api/security/FirebaseConfig.java
@@ -1,7 +1,9 @@
 package com.api.security;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.context.annotation.Configuration;
 
@@ -14,40 +16,40 @@ import jakarta.annotation.PostConstruct;
 @Configuration
 public class FirebaseConfig {
 	
-	 @PostConstruct
-	    public void init() throws IOException {
-	       InputStream serviceAccount = 
-	    		   getClass().getClassLoader().getResourceAsStream("celarisdistribucion-firebase-adminsdk-fbsvc-5eeb28dd97.json");
-
-	        FirebaseOptions options = FirebaseOptions.builder()
-	            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-	            .build();
-
-	        if (FirebaseApp.getApps().isEmpty()) {
-	            FirebaseApp.initializeApp(options);
-	        }
-	    }
+//	 @PostConstruct
+//	    public void init() throws IOException {
+//	       InputStream serviceAccount = 
+//	    		   getClass().getClassLoader().getResourceAsStream("celarisdistribucion-firebase-adminsdk-fbsvc-5eeb28dd97.json");
+//
+//	        FirebaseOptions options = FirebaseOptions.builder()
+//	            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//	            .build();
+//
+//	        if (FirebaseApp.getApps().isEmpty()) {
+//	            FirebaseApp.initializeApp(options);
+//	        }
+//	    }
 	 
 //	 PRODUCCION EN EL SERVIDOR DE RENDER
-//	 @PostConstruct
-//	 public void init() throws IOException {
-//	     InputStream serviceAccount;
-//	     
-//	     String envJson = System.getenv("FIREBASE_SERVICE_ACCOUNT_JSON");
-//	     if (envJson != null) {
-//	         // Producción: viene de variable de entorno
-//	         serviceAccount = new ByteArrayInputStream(envJson.getBytes(StandardCharsets.UTF_8));
-//	     } else {
-//	         // Local: lee del archivo
-//	         serviceAccount = getClass().getClassLoader()
-//	             .getResourceAsStream("firebase-service-account.json");
-//	     }
-//
-//	     FirebaseOptions options = FirebaseOptions.builder()
-//	         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//	         .build();
-//	     if (FirebaseApp.getApps().isEmpty()) {
-//	         FirebaseApp.initializeApp(options);
-//	     }
-//	 }
+	 @PostConstruct
+	 public void init() throws IOException {
+	     InputStream serviceAccount;
+	     
+	     String envJson = System.getenv("FIREBASE_SERVICE_ACCOUNT_JSON");
+	     if (envJson != null) {
+	         // Producción: viene de variable de entorno
+	         serviceAccount = new ByteArrayInputStream(envJson.getBytes(StandardCharsets.UTF_8));
+	     } else {
+	         // Local: lee del archivo
+	         serviceAccount = getClass().getClassLoader()
+	             .getResourceAsStream("firebase-service-account.json");
+	     }
+
+	     FirebaseOptions options = FirebaseOptions.builder()
+	         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+	         .build();
+	     if (FirebaseApp.getApps().isEmpty()) {
+	         FirebaseApp.initializeApp(options);
+	     }
+	 }
 }
