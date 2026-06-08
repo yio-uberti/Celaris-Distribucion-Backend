89644d670572c447e9e1418e410eb4145b35c914
yio-uberti <164667770+yio-uberti@users.noreply.github.com>
2026-06-08 14:54:49 -0300

    ajuste de error de google con cuentas eliminadas

--- src/main/java/com/api/user/Emails_eliminados.java
+++ src/main/java/com/api/user/Emails_eliminados.java
@@ -4,9 +4,6 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +13,6 @@ import lombok.Data;
 @Data
 @Builder
 public class Emails_eliminados {
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 	
 	@Column
 	private String email;
