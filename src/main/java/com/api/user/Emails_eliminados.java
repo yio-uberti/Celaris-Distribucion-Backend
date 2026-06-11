e234026e7688fe2f6a271cbc8e5762d0f455864c
yio-uberti <164667770+yio-uberti@users.noreply.github.com>
2026-06-11 16:27:25 -0300

    actualizando Entidades de borrado

--- src/main/java/com/api/user/Emails_eliminados.java
+++ src/main/java/com/api/user/Emails_eliminados.java
@@ -6,13 +6,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "emails_eliminados", schema = "", catalog = "")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Emails_eliminados {
 	
 	@Id
